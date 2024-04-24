5354c1e023ee436d092ea92b7a075e02d65e4040
yunfeng <yunfeng2024@gmail.com>
2024-04-24 15:07:24 -0700

    deletedcode

--- frontend/src/ManagerDetail.jsx
+++ frontend/src/ManagerDetail.jsx
@@ -34,36 +34,3 @@ export default function ManagerDetail() {
     </div>
   );
 }
-
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
-// import { useNavigate, useParams } from "react-router";
-
-// export default function PokemonDetail() {
-//   const [pokemonDetails, setPokemonDetails] = useState({});
-//   const navigate = useNavigate();
-//   const params = useParams();
-
-//   async function fetchAndSetPokemon() {
-//     const pokemonResponse = await axios.get("/api/pokemon/" + params.pokemonId);
-//     setPokemonDetails(pokemonResponse.data);
-//   }
-
-//   useEffect(function () {
-//     fetchAndSetPokemon();
-//   }, []);
-
-//   async function deletePokemon() {
-//     const response = await axios.delete("/api/pokemon/" + params.pokemonId);
-//     navigate("/");
-//   }
-
-//   return (
-//     <div>
-//       <div>Name: {pokemonDetails.name}</div>
-//       <div>Color: {pokemonDetails.color}</div>
-//       <div>Health: {pokemonDetails.health}</div>
-//       <button onClick={deletePokemon}>Delete Me!</button>
-//     </div>
-//   );
-// }
