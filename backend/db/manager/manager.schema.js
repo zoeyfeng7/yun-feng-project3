a56fe7dab5d2be50dc1a8219c63d5182c8bbcc3f
yunfeng <yunfeng2024@gmail.com>
2024-04-24 17:13:21 -0700

    deletedcode

--- backend/db/manager/manager.schema.js
+++ backend/db/manager/manager.schema.js
@@ -16,22 +16,3 @@ exports.ManagerSchema = new Schema(
   },
   { collection: "myManagerSpr2023" }
 );
-
-// const Schema = require("mongoose").Schema;
-
-// exports.PokemonSchema = new Schema(
-//   {
-//     name: String,
-//     health: Number,
-//     color: {
-//       type: String,
-//       default: "green",
-//       require: false,
-//     },
-//     username: {
-//       type: String,
-//       require: true,
-//     },
-//   },
-//   { collection: "myPokemonSpr2023" }
-// );
