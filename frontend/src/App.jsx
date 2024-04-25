513b32112f211d9da0a89da286df697e7ddcea1f
yunfeng <yunfeng2024@gmail.com>
2024-04-24 17:23:54 -0700

    deletedcode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Login";
-import CreateUser from "./CreateUser";
-import Managers from "./Managers";
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Login from "./Login";
+// import CreateUser from "./CreateUser";
+// import Managers from "./Managers";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/login" element={<Login />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
 
 // import { useState } from "react";
 // import axios from "axios";
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import Managers from "./Managers";
 import HomePage from "./HomePage";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
