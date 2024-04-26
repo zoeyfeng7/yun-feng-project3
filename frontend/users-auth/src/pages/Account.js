fd27685c35ada5bf067673170e18d7659900d277
yunfeng <yunfeng2024@gmail.com>
2024-04-25 18:26:45 -0700

    add search by website function

--- frontend/users-auth/src/pages/Account.js
+++ frontend/users-auth/src/pages/Account.js
@@ -3,7 +3,11 @@ import React from "react";
 function Account() {
   return (
     <div className="w-full h-screen bg-[#1a1a1a] text-white flex justify-center items-center">
-      <h2 className="text-3xl">ACCOUNT</h2>
+      <div className="AddingPassword">
+        <input type="text" placeholder="Ex. user123" />
+        <input type="text" placeholder="Ex. password" />
+        <button>Add Password</button>
+      </div>
     </div>
   );
 }
