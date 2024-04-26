54e6c6e3e37babb6532c6e5a06017c8d57a20a6e
yunfeng <yunfeng2024@gmail.com>
2024-04-26 16:34:19 -0700

    deleted unused code

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // Make sure to create a CSS file for styling
-
+import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
